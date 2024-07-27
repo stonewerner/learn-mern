1eb0457a530030e27df4c2c7d8d13f30e6e95103
Stone Werner <stonewerner@berkeley.edu>
2024-07-27 11:21:44 -0500

    get by id

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,13 @@ import { Project } from "./models/projectModel.js";
 const app = express();
 app.use(express.json()); //convert body of all requests to json
 
+// Route root
 app.get("/", (req, res) => {
     console.log(req);
     return res.status(200).send("Welcome!");
 });
 
+// Route to create a project
 app.post("/projects", async (req, res) => {
     try {
         if (
@@ -26,7 +28,6 @@ app.post("/projects", async (req, res) => {
             state: req.body.state,
             notes: req.body.notes,
         };
-
         const project = await Project.create(newProject);
         return res.status(200).send(project);
         
@@ -34,8 +35,34 @@ app.post("/projects", async (req, res) => {
         console.log(error.message);
         res.status(500).send({ message: error.message});
     }
-
 });
+
+// Route to get all projects
+app.get("/projects", async (req, res) => {
+    try {
+        const projects = await Project.find({});
+        return res.status(200).json({
+            count: projects.length,
+            data: projects
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({ message: error.message});
+    }
+});
+
+// Route to get a single project by id
+app.get("/projects/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const project = await Project.findById(id);
+        return res.status(200).json(project);
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({ message: error.message});
+    }
+})
 
 mongoose
     .connect(mongoDBURL)
